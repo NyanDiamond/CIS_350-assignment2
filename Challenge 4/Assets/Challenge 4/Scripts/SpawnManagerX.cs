2fa9275fe6d7af4254679a9d5d22f875ab729fe6
Josh Bonovich <joshua.bonovich@tkdtrio.com>
2022-11-10 17:16:04 -0600

    Challenge 4

---
+++ Challenge 4/Assets/Challenge 4/Scripts/SpawnManagerX.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+//Josh Bonovich
+//Challenge 4
+//This script controls the spawning
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+
+    private float spawnRangeX = 10;
+    private float spawnZMin = 15; // set min spawn Z
+    private float spawnZMax = 25; // set max spawn Z
+
+    public int enemyCount;
+    public int waveCount = 1;
+
+    public float speed = 8f;
+
+    public GameObject gameOverScreen;
+    public Text gameOverText;
+    public Text waveCounter;
+    public Text livesText;
+    public int lives = 10;
+
+    private bool gameOver = false;
+
+    public GameObject player;
+
+    private void Start()
+    {
+        Time.timeScale = 1f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameOver)
+        {
+            livesText.text = "Lives: " + lives;
+            if (lives <= 0)
+            {
+                GameOver();
+                gameOverText.text = "You Lose";
+                return;
+            }
+            enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+            if (enemyCount == 0)
+            {
+                if (waveCount > 10)
+                {
+                    GameOver();
+                    gameOverText.text = "You Win";
+                    return;
+                }
+                SpawnEnemyWave(waveCount);
+                waveCount++;
+                speed += 2f;
+            }
+        }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+
+    }
+
+    void GameOver()
+    {
+        Time.timeScale = 0f;
+        gameOver = true;
+        gameOverScreen.SetActive(true);
+    }
+
+    // Generate random spawn position for powerups and enemy balls
+    Vector3 GenerateSpawnPosition ()
+    {
+        float xPos = Random.Range(-spawnRangeX, spawnRangeX);
+        float zPos = Random.Range(spawnZMin, spawnZMax);
+        return new Vector3(xPos, 0, zPos);
+    }
+
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        waveCounter.text = "Wave: " + waveCount;
+        Vector3 powerupSpawnOffset = new Vector3(0, 0, -15); // make powerups spawn at player end
+
+        // If no powerups remain, spawn a powerup
+        if (GameObject.FindGameObjectsWithTag("Powerup").Length == 0) // check that there are zero powerups
+        {
+            Instantiate(powerupPrefab, GenerateSpawnPosition() + powerupSpawnOffset, powerupPrefab.transform.rotation);
+        }
+
+        // Spawn number of enemy balls based on wave number
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+
+        ResetPlayerPosition(); // put player back at start
+
+    }
+
+    // Move player back to position in front of own goal
+    void ResetPlayerPosition ()
+    {
+        player.transform.position = new Vector3(0, 1, -7);
+        player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        player.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+    }
+
+}
